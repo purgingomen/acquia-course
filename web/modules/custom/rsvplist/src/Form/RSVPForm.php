995db788586935b070d683371dcd3fcc5428088b
Rob Casey <note2self@gmail.com>
2023-09-18 11:19:27 -0400

    Initial setup of the RSVP module

---
+++ web/modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * @file
+ * A form to collect an email address for RSVP details.
+ */
+
+namespace Drupal\rsvplist\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class RSVPForm extends FormBase {
+    
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'rsvplist_email_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // Attempt to get the fully loaded node object of the viewed page.
+    $node = \Drupal::routeMatch()->getParameter('node');
+
+    // Some pages may not be nodes though and $node will be NULL on those pages.
+    // If a node was loaded, get the node id.
+    if (!(is_null($node))) {
+        $nid = $node->id();
+    }
+    else {
+        // if a node could not be loaded, default to 0;
+        $nid = 0;
+    }
+
+    // Establish the $form render array. It has an email text field,
+    // a submit button, and a hidden field containing the node ID.
+    $form['email'] = [
+      '#type' => 'textfield',
+      '#title' => t('Email address'),
+      '#size' => 25,
+      '#description' => t('We will send updates to the email address you provide.'),
+      '#required' => TRUE,
+    ];
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => t('RSVP'),
+    ];
+    $form['nid'] = [
+      '#type' => 'hidden',
+      '#value' => $nid
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $submitted_email = $form_state->getValue('email');
+    $this->messenger()->addMessage(t('The form is working! You entered @entry',
+      ['@entry' => $submitted_email])); 
+  }
+}
